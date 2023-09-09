0676eba205c8fa5b00fa1faa8446f3988f118843
suraj <sksuraj01@gmail.com>
2023-09-09 12:15:15 +0530

    ert

--- src/test/java/com/jenkins/maven/JenkinsGitMavenApplicationTests.java
+++ src/test/java/com/jenkins/maven/JenkinsGitMavenApplicationTests.java
@@ -1,16 +1,15 @@
 package com.jenkins.maven;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 class JenkinsGitMavenApplicationTests {
 	
 	
